64d4ff6e69bdec0266d284aa2adae15006041cb7
jie.zhang <1317674293@qq.com>
2019-07-26 08:37:00 +0800

    Fixed type

--- common/mavlink_msg_gas_sensor_value.h
+++ common/mavlink_msg_gas_sensor_value.h
@@ -5,26 +5,26 @@
 
 MAVPACKED(
 typedef struct __mavlink_gas_sensor_value_t {
- uint64_t PRESSURE; /*<  The unit is pa*/
  uint32_t SO2; /*<  The unit is ppb*/
  uint32_t VOC; /*<  The unit is ppb*/
  uint32_t NO2; /*<  The unit is ppb*/
  uint32_t CO; /*<  The unit is ppm*/
  uint32_t NH3; /*<  The unit is ppm*/
  uint32_t O3; /*<  The unit is ppb*/
+ uint32_t PRESSURE; /*<  The unit is pa*/
  uint16_t PM25; /*<  The unit is ug/cm3*/
  uint16_t PM10; /*<  The unit is ug/cm3*/
  uint16_t TEMPERATURE; /*<  The unit is C*/
  uint8_t HUMIDITY; /*<  The unit is %RH*/
 }) mavlink_gas_sensor_value_t;
 
-#define MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN 39
-#define MAVLINK_MSG_ID_GAS_SENSOR_VALUE_MIN_LEN 39
-#define MAVLINK_MSG_ID_10000_LEN 39
-#define MAVLINK_MSG_ID_10000_MIN_LEN 39
+#define MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN 35
+#define MAVLINK_MSG_ID_GAS_SENSOR_VALUE_MIN_LEN 35
+#define MAVLINK_MSG_ID_10000_LEN 35
+#define MAVLINK_MSG_ID_10000_MIN_LEN 35
 
-#define MAVLINK_MSG_ID_GAS_SENSOR_VALUE_CRC 250
-#define MAVLINK_MSG_ID_10000_CRC 250
+#define MAVLINK_MSG_ID_GAS_SENSOR_VALUE_CRC 108
+#define MAVLINK_MSG_ID_10000_CRC 108
 
 
 
@@ -33,34 +33,34 @@ typedef struct __mavlink_gas_sensor_value_t {
     10000, \
     "GAS_SENSOR_VALUE", \
     11, \
-    {  { "PM25", NULL, MAVLINK_TYPE_UINT16_T, 0, 32, offsetof(mavlink_gas_sensor_value_t, PM25) }, \
-         { "PM10", NULL, MAVLINK_TYPE_UINT16_T, 0, 34, offsetof(mavlink_gas_sensor_value_t, PM10) }, \
-         { "SO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 8, offsetof(mavlink_gas_sensor_value_t, SO2) }, \
-         { "VOC", NULL, MAVLINK_TYPE_UINT32_T, 0, 12, offsetof(mavlink_gas_sensor_value_t, VOC) }, \
-         { "NO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 16, offsetof(mavlink_gas_sensor_value_t, NO2) }, \
-         { "CO", NULL, MAVLINK_TYPE_UINT32_T, 0, 20, offsetof(mavlink_gas_sensor_value_t, CO) }, \
-         { "NH3", NULL, MAVLINK_TYPE_UINT32_T, 0, 24, offsetof(mavlink_gas_sensor_value_t, NH3) }, \
-         { "O3", NULL, MAVLINK_TYPE_UINT32_T, 0, 28, offsetof(mavlink_gas_sensor_value_t, O3) }, \
-         { "PRESSURE", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_gas_sensor_value_t, PRESSURE) }, \
-         { "HUMIDITY", NULL, MAVLINK_TYPE_UINT8_T, 0, 38, offsetof(mavlink_gas_sensor_value_t, HUMIDITY) }, \
-         { "TEMPERATURE", NULL, MAVLINK_TYPE_UINT16_T, 0, 36, offsetof(mavlink_gas_sensor_value_t, TEMPERATURE) }, \
+    {  { "PM25", NULL, MAVLINK_TYPE_UINT16_T, 0, 28, offsetof(mavlink_gas_sensor_value_t, PM25) }, \
+         { "PM10", NULL, MAVLINK_TYPE_UINT16_T, 0, 30, offsetof(mavlink_gas_sensor_value_t, PM10) }, \
+         { "SO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_gas_sensor_value_t, SO2) }, \
+         { "VOC", NULL, MAVLINK_TYPE_UINT32_T, 0, 4, offsetof(mavlink_gas_sensor_value_t, VOC) }, \
+         { "NO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 8, offsetof(mavlink_gas_sensor_value_t, NO2) }, \
+         { "CO", NULL, MAVLINK_TYPE_UINT32_T, 0, 12, offsetof(mavlink_gas_sensor_value_t, CO) }, \
+         { "NH3", NULL, MAVLINK_TYPE_UINT32_T, 0, 16, offsetof(mavlink_gas_sensor_value_t, NH3) }, \
+         { "O3", NULL, MAVLINK_TYPE_UINT32_T, 0, 20, offsetof(mavlink_gas_sensor_value_t, O3) }, \
+         { "PRESSURE", NULL, MAVLINK_TYPE_UINT32_T, 0, 24, offsetof(mavlink_gas_sensor_value_t, PRESSURE) }, \
+         { "HUMIDITY", NULL, MAVLINK_TYPE_UINT8_T, 0, 34, offsetof(mavlink_gas_sensor_value_t, HUMIDITY) }, \
+         { "TEMPERATURE", NULL, MAVLINK_TYPE_UINT16_T, 0, 32, offsetof(mavlink_gas_sensor_value_t, TEMPERATURE) }, \
          } \
 }
 #else
 #define MAVLINK_MESSAGE_INFO_GAS_SENSOR_VALUE { \
     "GAS_SENSOR_VALUE", \
     11, \
-    {  { "PM25", NULL, MAVLINK_TYPE_UINT16_T, 0, 32, offsetof(mavlink_gas_sensor_value_t, PM25) }, \
-         { "PM10", NULL, MAVLINK_TYPE_UINT16_T, 0, 34, offsetof(mavlink_gas_sensor_value_t, PM10) }, \
-         { "SO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 8, offsetof(mavlink_gas_sensor_value_t, SO2) }, \
-         { "VOC", NULL, MAVLINK_TYPE_UINT32_T, 0, 12, offsetof(mavlink_gas_sensor_value_t, VOC) }, \
-         { "NO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 16, offsetof(mavlink_gas_sensor_value_t, NO2) }, \
-         { "CO", NULL, MAVLINK_TYPE_UINT32_T, 0, 20, offsetof(mavlink_gas_sensor_value_t, CO) }, \
-         { "NH3", NULL, MAVLINK_TYPE_UINT32_T, 0, 24, offsetof(mavlink_gas_sensor_value_t, NH3) }, \
-         { "O3", NULL, MAVLINK_TYPE_UINT32_T, 0, 28, offsetof(mavlink_gas_sensor_value_t, O3) }, \
-         { "PRESSURE", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_gas_sensor_value_t, PRESSURE) }, \
-         { "HUMIDITY", NULL, MAVLINK_TYPE_UINT8_T, 0, 38, offsetof(mavlink_gas_sensor_value_t, HUMIDITY) }, \
-         { "TEMPERATURE", NULL, MAVLINK_TYPE_UINT16_T, 0, 36, offsetof(mavlink_gas_sensor_value_t, TEMPERATURE) }, \
+    {  { "PM25", NULL, MAVLINK_TYPE_UINT16_T, 0, 28, offsetof(mavlink_gas_sensor_value_t, PM25) }, \
+         { "PM10", NULL, MAVLINK_TYPE_UINT16_T, 0, 30, offsetof(mavlink_gas_sensor_value_t, PM10) }, \
+         { "SO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 0, offsetof(mavlink_gas_sensor_value_t, SO2) }, \
+         { "VOC", NULL, MAVLINK_TYPE_UINT32_T, 0, 4, offsetof(mavlink_gas_sensor_value_t, VOC) }, \
+         { "NO2", NULL, MAVLINK_TYPE_UINT32_T, 0, 8, offsetof(mavlink_gas_sensor_value_t, NO2) }, \
+         { "CO", NULL, MAVLINK_TYPE_UINT32_T, 0, 12, offsetof(mavlink_gas_sensor_value_t, CO) }, \
+         { "NH3", NULL, MAVLINK_TYPE_UINT32_T, 0, 16, offsetof(mavlink_gas_sensor_value_t, NH3) }, \
+         { "O3", NULL, MAVLINK_TYPE_UINT32_T, 0, 20, offsetof(mavlink_gas_sensor_value_t, O3) }, \
+         { "PRESSURE", NULL, MAVLINK_TYPE_UINT32_T, 0, 24, offsetof(mavlink_gas_sensor_value_t, PRESSURE) }, \
+         { "HUMIDITY", NULL, MAVLINK_TYPE_UINT8_T, 0, 34, offsetof(mavlink_gas_sensor_value_t, HUMIDITY) }, \
+         { "TEMPERATURE", NULL, MAVLINK_TYPE_UINT16_T, 0, 32, offsetof(mavlink_gas_sensor_value_t, TEMPERATURE) }, \
          } \
 }
 #endif
@@ -85,32 +85,32 @@ typedef struct __mavlink_gas_sensor_value_t {
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_gas_sensor_value_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                               uint16_t PM25, uint16_t PM10, uint32_t SO2, uint32_t VOC, uint32_t NO2, uint32_t CO, uint32_t NH3, uint32_t O3, uint64_t PRESSURE, uint8_t HUMIDITY, uint16_t TEMPERATURE)
+                               uint16_t PM25, uint16_t PM10, uint32_t SO2, uint32_t VOC, uint32_t NO2, uint32_t CO, uint32_t NH3, uint32_t O3, uint32_t PRESSURE, uint8_t HUMIDITY, uint16_t TEMPERATURE)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN];
-    _mav_put_uint64_t(buf, 0, PRESSURE);
-    _mav_put_uint32_t(buf, 8, SO2);
-    _mav_put_uint32_t(buf, 12, VOC);
-    _mav_put_uint32_t(buf, 16, NO2);
-    _mav_put_uint32_t(buf, 20, CO);
-    _mav_put_uint32_t(buf, 24, NH3);
-    _mav_put_uint32_t(buf, 28, O3);
-    _mav_put_uint16_t(buf, 32, PM25);
-    _mav_put_uint16_t(buf, 34, PM10);
-    _mav_put_uint16_t(buf, 36, TEMPERATURE);
-    _mav_put_uint8_t(buf, 38, HUMIDITY);
+    _mav_put_uint32_t(buf, 0, SO2);
+    _mav_put_uint32_t(buf, 4, VOC);
+    _mav_put_uint32_t(buf, 8, NO2);
+    _mav_put_uint32_t(buf, 12, CO);
+    _mav_put_uint32_t(buf, 16, NH3);
+    _mav_put_uint32_t(buf, 20, O3);
+    _mav_put_uint32_t(buf, 24, PRESSURE);
+    _mav_put_uint16_t(buf, 28, PM25);
+    _mav_put_uint16_t(buf, 30, PM10);
+    _mav_put_uint16_t(buf, 32, TEMPERATURE);
+    _mav_put_uint8_t(buf, 34, HUMIDITY);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN);
 #else
     mavlink_gas_sensor_value_t packet;
-    packet.PRESSURE = PRESSURE;
     packet.SO2 = SO2;
     packet.VOC = VOC;
     packet.NO2 = NO2;
     packet.CO = CO;
     packet.NH3 = NH3;
     packet.O3 = O3;
+    packet.PRESSURE = PRESSURE;
     packet.PM25 = PM25;
     packet.PM10 = PM10;
     packet.TEMPERATURE = TEMPERATURE;
@@ -144,32 +144,32 @@ static inline uint16_t mavlink_msg_gas_sensor_value_pack(uint8_t system_id, uint
  */
 static inline uint16_t mavlink_msg_gas_sensor_value_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
                                mavlink_message_t* msg,
-                                   uint16_t PM25,uint16_t PM10,uint32_t SO2,uint32_t VOC,uint32_t NO2,uint32_t CO,uint32_t NH3,uint32_t O3,uint64_t PRESSURE,uint8_t HUMIDITY,uint16_t TEMPERATURE)
+                                   uint16_t PM25,uint16_t PM10,uint32_t SO2,uint32_t VOC,uint32_t NO2,uint32_t CO,uint32_t NH3,uint32_t O3,uint32_t PRESSURE,uint8_t HUMIDITY,uint16_t TEMPERATURE)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN];
-    _mav_put_uint64_t(buf, 0, PRESSURE);
-    _mav_put_uint32_t(buf, 8, SO2);
-    _mav_put_uint32_t(buf, 12, VOC);
-    _mav_put_uint32_t(buf, 16, NO2);
-    _mav_put_uint32_t(buf, 20, CO);
-    _mav_put_uint32_t(buf, 24, NH3);
-    _mav_put_uint32_t(buf, 28, O3);
-    _mav_put_uint16_t(buf, 32, PM25);
-    _mav_put_uint16_t(buf, 34, PM10);
-    _mav_put_uint16_t(buf, 36, TEMPERATURE);
-    _mav_put_uint8_t(buf, 38, HUMIDITY);
+    _mav_put_uint32_t(buf, 0, SO2);
+    _mav_put_uint32_t(buf, 4, VOC);
+    _mav_put_uint32_t(buf, 8, NO2);
+    _mav_put_uint32_t(buf, 12, CO);
+    _mav_put_uint32_t(buf, 16, NH3);
+    _mav_put_uint32_t(buf, 20, O3);
+    _mav_put_uint32_t(buf, 24, PRESSURE);
+    _mav_put_uint16_t(buf, 28, PM25);
+    _mav_put_uint16_t(buf, 30, PM10);
+    _mav_put_uint16_t(buf, 32, TEMPERATURE);
+    _mav_put_uint8_t(buf, 34, HUMIDITY);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN);
 #else
     mavlink_gas_sensor_value_t packet;
-    packet.PRESSURE = PRESSURE;
     packet.SO2 = SO2;
     packet.VOC = VOC;
     packet.NO2 = NO2;
     packet.CO = CO;
     packet.NH3 = NH3;
     packet.O3 = O3;
+    packet.PRESSURE = PRESSURE;
     packet.PM25 = PM25;
     packet.PM10 = PM10;
     packet.TEMPERATURE = TEMPERATURE;
@@ -227,32 +227,32 @@ static inline uint16_t mavlink_msg_gas_sensor_value_encode_chan(uint8_t system_i
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_gas_sensor_value_send(mavlink_channel_t chan, uint16_t PM25, uint16_t PM10, uint32_t SO2, uint32_t VOC, uint32_t NO2, uint32_t CO, uint32_t NH3, uint32_t O3, uint64_t PRESSURE, uint8_t HUMIDITY, uint16_t TEMPERATURE)
+static inline void mavlink_msg_gas_sensor_value_send(mavlink_channel_t chan, uint16_t PM25, uint16_t PM10, uint32_t SO2, uint32_t VOC, uint32_t NO2, uint32_t CO, uint32_t NH3, uint32_t O3, uint32_t PRESSURE, uint8_t HUMIDITY, uint16_t TEMPERATURE)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN];
-    _mav_put_uint64_t(buf, 0, PRESSURE);
-    _mav_put_uint32_t(buf, 8, SO2);
-    _mav_put_uint32_t(buf, 12, VOC);
-    _mav_put_uint32_t(buf, 16, NO2);
-    _mav_put_uint32_t(buf, 20, CO);
-    _mav_put_uint32_t(buf, 24, NH3);
-    _mav_put_uint32_t(buf, 28, O3);
-    _mav_put_uint16_t(buf, 32, PM25);
-    _mav_put_uint16_t(buf, 34, PM10);
-    _mav_put_uint16_t(buf, 36, TEMPERATURE);
-    _mav_put_uint8_t(buf, 38, HUMIDITY);
+    _mav_put_uint32_t(buf, 0, SO2);
+    _mav_put_uint32_t(buf, 4, VOC);
+    _mav_put_uint32_t(buf, 8, NO2);
+    _mav_put_uint32_t(buf, 12, CO);
+    _mav_put_uint32_t(buf, 16, NH3);
+    _mav_put_uint32_t(buf, 20, O3);
+    _mav_put_uint32_t(buf, 24, PRESSURE);
+    _mav_put_uint16_t(buf, 28, PM25);
+    _mav_put_uint16_t(buf, 30, PM10);
+    _mav_put_uint16_t(buf, 32, TEMPERATURE);
+    _mav_put_uint8_t(buf, 34, HUMIDITY);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GAS_SENSOR_VALUE, buf, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_MIN_LEN, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_CRC);
 #else
     mavlink_gas_sensor_value_t packet;
-    packet.PRESSURE = PRESSURE;
     packet.SO2 = SO2;
     packet.VOC = VOC;
     packet.NO2 = NO2;
     packet.CO = CO;
     packet.NH3 = NH3;
     packet.O3 = O3;
+    packet.PRESSURE = PRESSURE;
     packet.PM25 = PM25;
     packet.PM10 = PM10;
     packet.TEMPERATURE = TEMPERATURE;
@@ -284,32 +284,32 @@ static inline void mavlink_msg_gas_sensor_value_send_struct(mavlink_channel_t ch
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_gas_sensor_value_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint16_t PM25, uint16_t PM10, uint32_t SO2, uint32_t VOC, uint32_t NO2, uint32_t CO, uint32_t NH3, uint32_t O3, uint64_t PRESSURE, uint8_t HUMIDITY, uint16_t TEMPERATURE)
+static inline void mavlink_msg_gas_sensor_value_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint16_t PM25, uint16_t PM10, uint32_t SO2, uint32_t VOC, uint32_t NO2, uint32_t CO, uint32_t NH3, uint32_t O3, uint32_t PRESSURE, uint8_t HUMIDITY, uint16_t TEMPERATURE)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
-    _mav_put_uint64_t(buf, 0, PRESSURE);
-    _mav_put_uint32_t(buf, 8, SO2);
-    _mav_put_uint32_t(buf, 12, VOC);
-    _mav_put_uint32_t(buf, 16, NO2);
-    _mav_put_uint32_t(buf, 20, CO);
-    _mav_put_uint32_t(buf, 24, NH3);
-    _mav_put_uint32_t(buf, 28, O3);
-    _mav_put_uint16_t(buf, 32, PM25);
-    _mav_put_uint16_t(buf, 34, PM10);
-    _mav_put_uint16_t(buf, 36, TEMPERATURE);
-    _mav_put_uint8_t(buf, 38, HUMIDITY);
+    _mav_put_uint32_t(buf, 0, SO2);
+    _mav_put_uint32_t(buf, 4, VOC);
+    _mav_put_uint32_t(buf, 8, NO2);
+    _mav_put_uint32_t(buf, 12, CO);
+    _mav_put_uint32_t(buf, 16, NH3);
+    _mav_put_uint32_t(buf, 20, O3);
+    _mav_put_uint32_t(buf, 24, PRESSURE);
+    _mav_put_uint16_t(buf, 28, PM25);
+    _mav_put_uint16_t(buf, 30, PM10);
+    _mav_put_uint16_t(buf, 32, TEMPERATURE);
+    _mav_put_uint8_t(buf, 34, HUMIDITY);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GAS_SENSOR_VALUE, buf, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_MIN_LEN, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_LEN, MAVLINK_MSG_ID_GAS_SENSOR_VALUE_CRC);
 #else
     mavlink_gas_sensor_value_t *packet = (mavlink_gas_sensor_value_t *)msgbuf;
-    packet->PRESSURE = PRESSURE;
     packet->SO2 = SO2;
     packet->VOC = VOC;
     packet->NO2 = NO2;
     packet->CO = CO;
     packet->NH3 = NH3;
     packet->O3 = O3;
+    packet->PRESSURE = PRESSURE;
     packet->PM25 = PM25;
     packet->PM10 = PM10;
     packet->TEMPERATURE = TEMPERATURE;
@@ -332,7 +332,7 @@ static inline void mavlink_msg_gas_sensor_value_send_buf(mavlink_message_t *msgb
  */
 static inline uint16_t mavlink_msg_gas_sensor_value_get_PM25(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint16_t(msg,  32);
+    return _MAV_RETURN_uint16_t(msg,  28);
 }
 
 /**
@@ -342,7 +342,7 @@ static inline uint16_t mavlink_msg_gas_sensor_value_get_PM25(const mavlink_messa
  */
 static inline uint16_t mavlink_msg_gas_sensor_value_get_PM10(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint16_t(msg,  34);
+    return _MAV_RETURN_uint16_t(msg,  30);
 }
 
 /**
@@ -352,7 +352,7 @@ static inline uint16_t mavlink_msg_gas_sensor_value_get_PM10(const mavlink_messa
  */
 static inline uint32_t mavlink_msg_gas_sensor_value_get_SO2(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint32_t(msg,  8);
+    return _MAV_RETURN_uint32_t(msg,  0);
 }
 
 /**
@@ -362,7 +362,7 @@ static inline uint32_t mavlink_msg_gas_sensor_value_get_SO2(const mavlink_messag
  */
 static inline uint32_t mavlink_msg_gas_sensor_value_get_VOC(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint32_t(msg,  12);
+    return _MAV_RETURN_uint32_t(msg,  4);
 }
 
 /**
@@ -372,7 +372,7 @@ static inline uint32_t mavlink_msg_gas_sensor_value_get_VOC(const mavlink_messag
  */
 static inline uint32_t mavlink_msg_gas_sensor_value_get_NO2(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint32_t(msg,  16);
+    return _MAV_RETURN_uint32_t(msg,  8);
 }
 
 /**
@@ -382,7 +382,7 @@ static inline uint32_t mavlink_msg_gas_sensor_value_get_NO2(const mavlink_messag
  */
 static inline uint32_t mavlink_msg_gas_sensor_value_get_CO(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint32_t(msg,  20);
+    return _MAV_RETURN_uint32_t(msg,  12);
 }
 
 /**
@@ -392,7 +392,7 @@ static inline uint32_t mavlink_msg_gas_sensor_value_get_CO(const mavlink_message
  */
 static inline uint32_t mavlink_msg_gas_sensor_value_get_NH3(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint32_t(msg,  24);
+    return _MAV_RETURN_uint32_t(msg,  16);
 }
 
 /**
@@ -402,7 +402,7 @@ static inline uint32_t mavlink_msg_gas_sensor_value_get_NH3(const mavlink_messag
  */
 static inline uint32_t mavlink_msg_gas_sensor_value_get_O3(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint32_t(msg,  28);
+    return _MAV_RETURN_uint32_t(msg,  20);
 }
 
 /**
@@ -410,9 +410,9 @@ static inline uint32_t mavlink_msg_gas_sensor_value_get_O3(const mavlink_message
  *
  * @return  The unit is pa
  */
-static inline uint64_t mavlink_msg_gas_sensor_value_get_PRESSURE(const mavlink_message_t* msg)
+static inline uint32_t mavlink_msg_gas_sensor_value_get_PRESSURE(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint64_t(msg,  0);
+    return _MAV_RETURN_uint32_t(msg,  24);
 }
 
 /**
@@ -422,7 +422,7 @@ static inline uint64_t mavlink_msg_gas_sensor_value_get_PRESSURE(const mavlink_m
  */
 static inline uint8_t mavlink_msg_gas_sensor_value_get_HUMIDITY(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint8_t(msg,  38);
+    return _MAV_RETURN_uint8_t(msg,  34);
 }
 
 /**
@@ -432,7 +432,7 @@ static inline uint8_t mavlink_msg_gas_sensor_value_get_HUMIDITY(const mavlink_me
  */
 static inline uint16_t mavlink_msg_gas_sensor_value_get_TEMPERATURE(const mavlink_message_t* msg)
 {
-    return _MAV_RETURN_uint16_t(msg,  36);
+    return _MAV_RETURN_uint16_t(msg,  32);
 }
 
 /**
@@ -444,13 +444,13 @@ static inline uint16_t mavlink_msg_gas_sensor_value_get_TEMPERATURE(const mavlin
 static inline void mavlink_msg_gas_sensor_value_decode(const mavlink_message_t* msg, mavlink_gas_sensor_value_t* gas_sensor_value)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    gas_sensor_value->PRESSURE = mavlink_msg_gas_sensor_value_get_PRESSURE(msg);
     gas_sensor_value->SO2 = mavlink_msg_gas_sensor_value_get_SO2(msg);
     gas_sensor_value->VOC = mavlink_msg_gas_sensor_value_get_VOC(msg);
     gas_sensor_value->NO2 = mavlink_msg_gas_sensor_value_get_NO2(msg);
     gas_sensor_value->CO = mavlink_msg_gas_sensor_value_get_CO(msg);
     gas_sensor_value->NH3 = mavlink_msg_gas_sensor_value_get_NH3(msg);
     gas_sensor_value->O3 = mavlink_msg_gas_sensor_value_get_O3(msg);
+    gas_sensor_value->PRESSURE = mavlink_msg_gas_sensor_value_get_PRESSURE(msg);
     gas_sensor_value->PM25 = mavlink_msg_gas_sensor_value_get_PM25(msg);
     gas_sensor_value->PM10 = mavlink_msg_gas_sensor_value_get_PM10(msg);
     gas_sensor_value->TEMPERATURE = mavlink_msg_gas_sensor_value_get_TEMPERATURE(msg);
